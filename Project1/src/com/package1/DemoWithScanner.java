4051ba943b8e604c9928ef7f22d24d4f1670049a
Manisha <sehgalmanisha0@gmail.com>
2020-11-21 14:10:31 +0530

    Pushing from eclipse for the first time

---
+++ Project1/src/com/package1/DemoWithScanner.java
@@ -0,0 +1,21 @@
+package com.package1;
+
+import java.util.Scanner;
+
+public class DemoWithScanner {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int sum= a+b;
+		
+		System.out.println("Sum is: "+sum);
+
+	}
+
+}
